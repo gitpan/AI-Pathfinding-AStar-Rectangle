328c57eb9881cc45362e56900c23a6074e2c6d3d
Анатолий Гришаев <grian@cpan.org>
2010-10-12 08:22:21 +0000

    Import of GRIAN/AI-Pathfinding-AStar-Rectangle-0.23 from CPAN.
    gitpan-cpan-distribution: AI-Pathfinding-AStar-Rectangle
    gitpan-cpan-version:      0.23
    gitpan-cpan-path:         GRIAN/AI-Pathfinding-AStar-Rectangle-0.23.tar.gz
    gitpan-cpan-author:       GRIAN
    gitpan-cpan-maturity:     released

--- lib/AI/Pathfinding/AStar/Rectangle.pm
+++ lib/AI/Pathfinding/AStar/Rectangle.pm
@@ -22,7 +22,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(	
 );
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 require XSLoader;
 XSLoader::load('AI::Pathfinding::AStar::Rectangle', $VERSION);
